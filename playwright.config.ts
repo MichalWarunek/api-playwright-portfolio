c8741afcb0748bd8241cb492b1fa522d45450cee
Michał Warunek <michal.warunek@gmail.com>
2026-06-03 17:13:01 +0200

    fix: fix webServer path to restful-booker application

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'cd ../restfulBooker/restful-booker && npm start',
+    command: 'cd ../restful-booker && npm start',
     url: 'http://localhost:3001',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
