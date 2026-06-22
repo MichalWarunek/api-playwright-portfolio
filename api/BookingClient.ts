cc1e2535cadb0d5f14f6b6ff85212ae087fce03d
Michał Warunek <michal.warunek@gmail.com>
2026-06-22 17:03:47 +0200

    test: add PUT booking test and refactor auth handling

--- api/BookingClient.ts
+++ api/BookingClient.ts
@@ -1,5 +1,14 @@
 import { APIRequestContext } from '@playwright/test';
 
+interface BookingInterface {
+  firstname: string,
+  lastname: string,
+  totalprice: number,
+  depositpaid: boolean,
+  bookingdates: { checkin: string, checkout: string },
+  additionalneeds: string
+}
+
 export class BookingClient {
     private request: APIRequestContext;
     constructor(request: APIRequestContext) {
@@ -11,6 +20,17 @@ export class BookingClient {
     async getBooking(id: string) {
         return await this.request.get(`/booking/${id}`);
     }
+    async login() {
+      const response = await this.request.post('/auth', {
+        data: {
+        "username": process.env.USER_NAME,
+        "password": process.env.USER_PASSWORD
+        }
+      });
+      const body = await response.json();
+          const token = body.token;
+          return token;
+    }
     async postBooking(bookingData: object) {
           const response = await this.request.post('/booking', {
             data: bookingData
@@ -19,6 +39,14 @@ export class BookingClient {
           const generatedId = body.bookingid;
           return generatedId;
     }
+    async updateBooking(bookingId: string, token: string, bookingData: BookingInterface) {
+      return await this.request.put(`/booking/${bookingId}`, {
+        data: bookingData,
+        headers: {
+          'Cookie': `token=${token}`
+        }
+      });
+}
     async postBookingRaw(bookingData: object) {
         return await this.request.post('/booking', {
           data: bookingData
---
+++ tests/api/updateBooking.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from "../../fixtures";
+import bookingData from "../../test-data/booking-data.json";
+
+test.describe('test PUT responses for bookings', () => {
+  let bookingId: string;
+  let token: string;
+  
+  test.beforeAll(async ({ bookingClient }) => {
+    bookingId = await bookingClient.postBooking(bookingData.bookingPayload);
+    token = await bookingClient.login();
+  });
+
+  test.afterAll(async ({ bookingClient }) => {
+    if (bookingId) {
+     await bookingClient.deleteBooking(bookingId);
+    }
+  });
+  
+  test('Should update particular booking ID', async ({ bookingClient }) => {
+    const bookingPayload = await bookingClient.getBooking(bookingId);
+    expect(bookingPayload.status()).toBe(200);
+    const body = await bookingPayload.json();
+    expect(body).toMatchObject(bookingData.bookingPayload);
+    const response = await bookingClient.updateBooking(bookingId, token, bookingData.bookingUpdate)
+    expect(response.status()).toBe(200);
+    const currentBooking = await response.json();
+    expect(currentBooking).toMatchObject(bookingData.bookingUpdate);
+  });
+});
